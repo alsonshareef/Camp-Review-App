3f2c3c1e78e5448f9b615cb352153526df18bc25
Alson Shareef <alson.shareef@gmail.com>
2018-03-22 00:05:20 +0800

    Added POST route and a form page

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+var campgrounds = [
+    {name: "Lane Poole Reserve", image:"https://pixabay.com/get/ea34b00e21f6003ed1584d05fb1d4e97e07ee3d21cac104497f1c77aa4edb0ba_340.jpg"},
+    {name: "Yalgorup National Park", image:"https://farm8.staticflickr.com/7296/28070862692_32f82c02ba.jpg"},
+    {name: "Wellington National Park", image:"https://pixabay.com/get/ea35b8062cf1063ed1584d05fb1d4e97e07ee3d21cac104497f1c77da7e9b4bb_340.jpg"}
+]
 
 // ROUTES ============================================
+// GET ===============================================
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name: "Lane Poole Reserve", image:"https://pixabay.com/get/ea34b00e21f6003ed1584d05fb1d4e97e07ee3d21cac104497f1c77aa4edb0ba_340.jpg"},
-        {name: "Yalgorup National Park", image:"https://farm8.staticflickr.com/7296/28070862692_32f82c02ba.jpg"},
-        {name: "Wellington National Park", image:"https://pixabay.com/get/ea35b8062cf1063ed1584d05fb1d4e97e07ee3d21cac104497f1c77da7e9b4bb_340.jpg"}
-    ]
-
     res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs");
+});
+
+// POST ===============================================
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image}
+    campgrounds.push(newCampground);
+
+    res.redirect("/campgrounds");
 });
 
 // ====================================================
