fd21db39ebbfd2bdcecbc5cf21009cd7132b569b
Alson Shareef <alson.shareef@gmail.com>
2018-03-22 18:13:20 +0800

    Initial styling

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 var campgrounds = [
     {name: "Lane Poole Reserve", image:"https://pixabay.com/get/ea34b00e21f6003ed1584d05fb1d4e97e07ee3d21cac104497f1c77aa4edb0ba_340.jpg"},
     {name: "Yalgorup National Park", image:"https://farm8.staticflickr.com/7296/28070862692_32f82c02ba.jpg"},
+    {name: "Wellington National Park", image:"https://pixabay.com/get/ea35b8062cf1063ed1584d05fb1d4e97e07ee3d21cac104497f1c77da7e9b4bb_340.jpg"},
+    {name: "Lane Poole Reserve", image:"https://pixabay.com/get/ea34b00e21f6003ed1584d05fb1d4e97e07ee3d21cac104497f1c77aa4edb0ba_340.jpg"},
+    {name: "Yalgorup National Park", image:"https://farm8.staticflickr.com/7296/28070862692_32f82c02ba.jpg"},
+    {name: "Wellington National Park", image:"https://pixabay.com/get/ea35b8062cf1063ed1584d05fb1d4e97e07ee3d21cac104497f1c77da7e9b4bb_340.jpg"},
+    {name: "Lane Poole Reserve", image:"https://pixabay.com/get/ea34b00e21f6003ed1584d05fb1d4e97e07ee3d21cac104497f1c77aa4edb0ba_340.jpg"},
+    {name: "Yalgorup National Park", image:"https://farm8.staticflickr.com/7296/28070862692_32f82c02ba.jpg"},
     {name: "Wellington National Park", image:"https://pixabay.com/get/ea35b8062cf1063ed1584d05fb1d4e97e07ee3d21cac104497f1c77da7e9b4bb_340.jpg"}
 ]
 
