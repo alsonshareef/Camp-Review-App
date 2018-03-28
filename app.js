932a2315d2aa2d846a3ef566e225f693744bbd28
Alson Shareef <alson.shareef@gmail.com>
2018-03-28 23:39:14 +0800

    Adding SHOW Route to display additional info of selected campsites

--- app.js
+++ app.js
@@ -11,42 +11,56 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Initial SCHEMA Setup
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 
 // ROUTES ============================================
-// GET ===============================================
 app.get("/", function(req, res){
     res.render("landing");
 });
 
+// INDEX ROUTE (shows all campgrounds)
 app.get("/campgrounds", function(req, res){
     Campground.find({}, function(err, allCampgrounds){
         if (err) {
             console.log(err);
         } else {
-            res.render("campgrounds", {campgrounds:allCampgrounds});
+            res.render("index", {campgrounds:allCampgrounds});
         }
     });
 });
 
+// NEW ROUTE (shows form to input a new campground)
 app.get("/campgrounds/new", function(req, res){
     res.render("new.ejs");
 });
 
-// POST ===============================================
+// CREATE ROUTE (adds new campground to db, and redirects to index route)
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image}
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc}
     Campground.create(newCampground, function(err, newlyCreated){
         if (err) {
             console.log(err);
         } else {
             res.redirect("/campgrounds");
+        }
+    });
+});
+
+// SHOW ROUTE (shows more info about a selected campground)
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("show", {campground: foundCampground});
         }
     });
 });
